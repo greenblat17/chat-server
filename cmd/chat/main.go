b8b58450e88e425f8d078485f1282127687f4f47
Aleksandr.Zhuravlev <Aleksandr.Zhuravlev@nexign.com>
2024-10-05 11:04:51 +0300

    feat(HW-1): add chat api

--- cmd/chat/main.go
+++ cmd/chat/main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/golang/protobuf/ptypes/empty"
+	desc "github.com/greenblat17/chat-server/pkg/chat_v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+const (
+	grpcPort = 50052
+)
+
+type server struct {
+	desc.UnimplementedChatV1Server
+}
+
+func (s *server) Create(_ context.Context, _ *desc.CreateRequest) (*desc.CreateResponse, error) {
+	return &desc.CreateResponse{
+		Id: 1,
+	}, nil
+}
+
+func (s *server) Delete(_ context.Context, _ *desc.DeleteRequest) (*empty.Empty, error) {
+	return &empty.Empty{}, nil
+}
+
+func (s *server) SendMessage(_ context.Context, _ *desc.SendMessageRequest) (*empty.Empty, error) {
+	return &empty.Empty{}, nil
+}
 
 func main() {
-	fmt.Println("Hello World")
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+	desc.RegisterChatV1Server(s, &server{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to server: %v", err)
+	}
 }
